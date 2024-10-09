ae23d8ae8888c27de9cf327d4b53a6b4708731a9
Andrew Naumann <35020306+AndresNaumann@users.noreply.github.com>
2024-10-09 11:15:58 -0600

    new developments

--- server.mjs
+++ server.mjs
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 import http from "http";
+import fs from "fs";
 
 dotenv.config();
 
@@ -14,13 +15,49 @@ app.use(bodyParser.json());
 const openai = new OpenAI({ apiKey: process.env.apiKey });
 
 let chatHistory = [];
+let messagesLDS = [];
+
+// USE A JSON FILE TO LOAD CONTENT INTO THE CHAT
+
+function loadContent() {
+  fs.readFile("bom.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading chat history:", err);
+      return;
+    }
+    try {
+      const scriptures = JSON.parse(data);
+
+      scriptures.forEach((p) => {
+        messagesLDS.push(p);
+      });
+
+      console.log("Chat history loaded successfully");
+    } catch (error) {
+      console.error("Error parsing chat history:", error);
+    }
+  });
+}
+
+loadContent();
+
+// MAIN FUNCTION
 
 async function main(text, res) {
+  // Map the chat history to the format OpenAI expects
+
   try {
     const messages = chatHistory.map(([role, content]) => ({
       role,
       content,
     }));
+
+    const loadedContext = messagesLDS.map(({ role, content }) => ({
+      role,
+      content,
+    }));
+
+    // Add the user message to the chat history
 
     messages.push({
       role: "user",
@@ -29,17 +66,38 @@ async function main(text, res) {
         ". Respond in the language that the previous sentence uses. Keep the response very brief.",
     });
 
+    loadedContext.push({
+      role: "user",
+      content:
+        text + ". Keep the response brief and use the preloaded JSON data.",
+    });
+
+    // Generate a completion based on the chat history
+
     const completion = await openai.chat.completions.create({
       messages: messages,
       model: "gpt-4o-mini",
     });
 
+    const completionLDS = await openai.chat.completions.create({
+      messages: loadedContext,
+      model: "gpt-4o-mini",
+    });
+
+    // Extract the completed text from the completion
+
     const completedText = completion.choices[0].message.content;
+    const completedTextLDS = completionLDS.choices[0].message.content;
+
+    // Add the completed text to the chat history
 
     chatHistory.push(["user", text]);
     chatHistory.push(["assistant", completedText]);
 
-    console.log(completedText);
+    messagesLDS.push({ role: "user", content: text });
+    messagesLDS.push({ role: "assistant", content: completedTextLDS });
+
+    console.log("LDS VERSION " + completedTextLDS);
 
     const mp3 = await openai.audio.speech.create({
       model: "tts-1",
@@ -72,7 +130,7 @@ async function main(text, res) {
     const options = {
       hostname: "localhost",
       port: 3001,
-      path: "/your-endpoint-path", // Adjust this path as needed
+      path: "/", // Adjust this path as needed
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -104,6 +162,8 @@ async function main(text, res) {
     res.status(500).send("Server error");
   }
 }
+
+// Define the route for the server
 
 app.post("/", (req, res) => {
   let text = req.body.text;
